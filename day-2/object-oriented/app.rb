8259e7dd038fa21c6b23f43c5cdceb9b26a7ea96
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-08-16 11:48:22 -0400

    Object Oriented: Class methods

--- day-2/object-oriented/app.rb
+++ day-2/object-oriented/app.rb
@@ -20,6 +20,17 @@ class Spaceship
     puts "The #{@name} is now taking off going at #{@speed} lightspeed. Shwooooooh."
   end
 
+
+  # CLASS METHOD
+
+  def self.star_trek_opening
+    puts "Space... the final frontier. These are the voyages of the starship Enterprise..."
+  end
+
+  def self.star_wars_opening
+    puts "A long time ago in a galaxy far far away. (cue John Williams music)"
+  end
+
 end
 
 
@@ -33,3 +44,13 @@ the_falcon.take_off
 
 the_enterprise = Spaceship.new("Enterprise", 2.0, "large")
 the_enterprise.take_off
+
+
+# NOPE: the_falcon.star_wars_opening
+
+puts ""
+puts "Class Methods:"
+
+Spaceship.star_wars_opening
+Spaceship.star_trek_opening
+
