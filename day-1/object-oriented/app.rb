eccd52fab3c38d3fb7c816efc0f20a75af52c56b
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-08-16 11:29:56 -0400

    Object Oriented: Add contructor to spaceship

--- day-1/object-oriented/app.rb
+++ day-1/object-oriented/app.rb
@@ -2,10 +2,24 @@
 #  ------------------------- SETUP -------------------------
 #  ---------------------------------------------------------/
 
+# shape
+# color
+# crew
+# captain
+# orders
+
 class Spaceship
-  def take_off
-    puts "The spaceship is now taking off. Shwooooooh."
+
+  def initialize(name, speed, size)
+    @name = name
+    @speed = speed
+    @size = size
   end
+
+  def take_off
+    puts "The #{@name} is now taking off going at #{@speed} lightspeed. Shwooooooh."
+  end
+
 end
 
 
@@ -14,6 +28,8 @@ end
 #  -------------------- APPLICATION CODE -------------------
 #  ---------------------------------------------------------/
 
-the_spaceship = Spaceship.new
+the_falcon = Spaceship.new("Millennium Falcon", 1.5, "medium")
+the_falcon.take_off
 
-the_spaceship.take_off
+the_enterprise = Spaceship.new("Enterprise", 2.0, "large")
+the_enterprise.take_off
