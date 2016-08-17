1f2c0aa677bc4fb0b9987cc2b5f3cd49f4757523
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-08-17 10:15:19 -0400

    Payroll: Refactor into separate files

--- solutions/day-2/payroll/app.rb
+++ solutions/day-2/payroll/app.rb
@@ -1,76 +1,9 @@
-# /---------------------------------------------------------
-#  ------------------------- SETUP -------------------------
-#  ---------------------------------------------------------/
+# app.rb
 
-class Employee
-    attr_reader :name, :email
-    def initialize(name, email)
-      @name = name
-      @email = email
-    end
-end
-
-# ----------
-
-class HourlyEmployee < Employee
-    def initialize(name, email, hourly_rate, hours_worked)
-        @name = name
-        @email = email
-        @hourly_rate = hourly_rate
-        @hours_worked = hours_worked
-    end
-
-    def calculate_salary
-      @hours_worked * @hourly_rate
-    end
-end
-
-# ----------
-
-class SalariedEmployee < Employee
-    def initialize(name, email, salary)
-        @name = name
-        @email = email
-        @salary = salary
-    end
-
-    def calculate_salary
-      exact_pay = @salary / 52.0
-      exact_pay.round(2)
-    end
-end
-
-# ----------
-
-class MultiPaymentEmployee < Employee
-    def initialize(name, email, salary, hourly_rate, hours_worked)
-        @name = name
-        @email = email
-        @salary = salary
-        @hourly_rate = hourly_rate
-        @hours_worked = hours_worked
-
-    end
-
-    def calculate_salary
-      exact_base_pay = @salary / 52.0
-      base_pay = exact_base_pay.round(2)
-
-      if @hours_worked > 40
-        payed_hours = @hours_worked - 40
-        extra_money = payed_hours * @hourly_rate
-        base_pay += extra_money
-      end
-
-      base_pay
-    end
-end
-
-
-
-# /---------------------------------------------------------
-#  ------------------- APPLICATION CODE --------------------
-#  ---------------------------------------------------------/
+require_relative("lib/employee.rb")
+require_relative("lib/hourly_employee.rb")
+require_relative("lib/salaried_employee.rb")
+require_relative("lib/multi_payment_employee.rb")
 
 
 josh = HourlyEmployee.new('Josh', 'nachoemail@example.com', 35, 50)
