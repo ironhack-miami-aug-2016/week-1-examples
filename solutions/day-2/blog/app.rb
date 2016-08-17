da53ba473f295fb971ef5df842b4a130d13d82f2
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-08-17 10:06:00 -0400

    Blog: Refactor into separate files

--- solutions/day-2/blog/app.rb
+++ solutions/day-2/blog/app.rb
@@ -1,98 +1,10 @@
-# /---------------------------------------------------------
-#  ------------------------- SETUP -------------------------
-#  ---------------------------------------------------------/
+# app.rb
 
-class Blog
-  def initialize
-    @posts = []
-    @page_start = 0
-    @page_end = 2
-  end
+require_relative("lib/blog.rb")
+require_relative("lib/post.rb")
 
-  def add_post(new_post)
-    @posts.push(new_post)
-  end
-
-  def go_next
-    # 0..2   ->   3..5
-    @page_start += 3
-    @page_end += 3
-  end
-
-  def go_prev
-    # 6..8   ->   3..5
-    @page_start -= 3
-    @page_end -= 3
-  end
-
-  def print_current_page
-    puts ""
-
-    # @page_start = 6
-    # @page_end = 8
-    current_posts = @posts[@page_start..@page_end]
-    # @posts[6..8]
-
-    current_posts.each do |the_post|
-      the_post.print
-    end
-  end
-end
-
-# ----------
-
-class Post
-  attr_accessor(:title, :date, :text)
-
-  def initialize(title, date, text)
-    @title = title
-    @date = date
-    @text = text
-  end
-
-  def print
-    puts @title
-    puts "------------"
-    puts @text
-    puts ""
-    puts "<=================>"
-    puts ""
-  end
-end
-
-# ----------
 
 seconds_in_day = 60 * 60 * 24
-
-# Examples of using Time:
-
-# 78 hours ago:
-seventy_eight_hours = Time.now + 78 * 60 * 60
-
-# Tomorrow:
-tomorrow = Time.now + 24 * 60 * 60
-
-# Displaying the time in a pretty format
-tomorrow.strftime("Tomorrow is: %A, %B %-d %Y")
-
-
-
-# /---------------------------------------------------------
-#  ------------------- APPLICATION CODE --------------------
-#  ---------------------------------------------------------/
-
-
-# posts = [ 0, 1, 2,    3, 4, 5,     6, 7, 8,     9, 10, 11,    12, 13 ]
-#           -------     -------      -------      ---------     ------
-
-# posts.slice(6, 3)
-
-#   0    1    2    3    4    5    6    7
-# ["a", "b", "c", "d", "e", "f", "g", "h"]
-#                           -------------
-
-# posts.slice(5, 3)
-
 
 the_blog = Blog.new
 the_blog.add_post Post.new("First", Time.now - 4 * seconds_in_day, "First text")
@@ -127,3 +39,30 @@ while input != "exit"
 
   the_blog.print_current_page
 end
+
+
+
+
+
+# Examples of using Time:
+
+# 78 hours ago:
+seventy_eight_hours = Time.now + 78 * 60 * 60
+
+# Tomorrow:
+tomorrow = Time.now + 24 * 60 * 60
+
+# Displaying the time in a pretty format
+tomorrow.strftime("Tomorrow is: %A, %B %-d %Y")
+
+
+# posts = [ 0, 1, 2,    3, 4, 5,     6, 7, 8,     9, 10, 11,    12, 13 ]
+#           -------     -------      -------      ---------     ------
+
+# posts.slice(6, 3)
+
+#   0    1    2    3    4    5    6    7
+# ["a", "b", "c", "d", "e", "f", "g", "h"]
+#                           -------------
+
+# posts.slice(5, 3)
